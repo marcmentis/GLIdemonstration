fcf045902eee8ae01bf4fc8f61e6e7fee3b21824
Marc Mentis <mmentis@optonline.net>
2015-02-16 17:39:26 -0500

    Only lib

--- lib/todo.rb
+++ lib/todo.rb
@@ -1,5 +1,6 @@
 require 'todo/version.rb'
 require 'todo/task.rb'
+require 'rainbow'
 
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
